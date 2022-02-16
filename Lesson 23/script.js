2115223c3222853be0977faaa4951dea32f4c4f2
Alokin26 <galaxyusert585@gmail.com>
2022-02-16 17:39:15 +0200

    Lesson 25

--- Lesson 23/script.js
+++ Lesson 23/script.js
@@ -1,5 +1,10 @@
-(function(){
-    $('.mobile-menu__link').on('click', function(){ 
-        $('.mobile-menu__container').hide(); 
-      });
-    });
+const mainNavLink= document.querySelector('.main-nav__link') 
+const mainNav = document.querySelector('.main-nav')
+
+function Click() { 
+mainNavLink.on('click', function(){  
+    mainNav.hide();  
+  }); 
+}
+
+Click()
