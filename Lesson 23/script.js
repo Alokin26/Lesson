7335e1d7e35b79855c92fe8039039c9e33353f31
Alokin26 <galaxyusert585@gmail.com>
2022-02-16 17:23:06 +0200

    Lesson 25

--- Lesson 23/script.js
+++ Lesson 23/script.js
@@ -1,13 +1,5 @@
-$('.faq_item_title_inner').on('click',function(){
-  $(this).parents('.faq_item').find('.faq_item_body').slideToggle(300);
-  $(this).toggleClass('open');
-  if ($(this).hasClass('show_all')){
-          if ($(this).hasClass('open')) {
-              $(this).html('Свернуть все');
-          $('.faq_item_title_inner:not(.open)').trigger('click');
-      } else {
-          $(this).html('Смотреть все');
-          $('.faq_item_title_inner.open').trigger('click');
-      }
-  }
-});
+(function(){
+    $('.mobile-menu__link').on('click', function(){ 
+        $('.mobile-menu__container').hide(); 
+      });
+    });
